c1cfc3bbcd7196a3b89ad8d37d67f5ca7a966352
Izaias Oliveira <izaiasneto.dev@gmail.com>
2026-02-03 11:00:30 -0300

    Add auto-submit setting toggle

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,6 +2,7 @@ class SettingsController < ApplicationController
   def edit
     @repos_folder = Setting.repos_folder
     @default_cli_client = Setting.default_cli_client
+    @auto_submit_enabled = Setting.auto_submit_enabled?
   end
 
   def update
@@ -15,6 +16,7 @@ class SettingsController < ApplicationController
 
     Setting.repos_folder = folder_path if folder_path.present?
     Setting.default_cli_client = cli_client if cli_client.present?
+    Setting.auto_submit_enabled = params[:auto_submit_enabled] == "1"
 
     redirect_to edit_settings_path, notice: "Settings updated"
   end
