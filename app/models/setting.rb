8a8ec2d749566e54b2ea609c2d11271c218f23d3
Izaias Oliveira <izaiasneto.dev@gmail.com>
2026-02-03 10:15:59 -0300

    Add auto-review settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -5,10 +5,15 @@ class Setting < ApplicationRecord
   CURRENT_REPO_KEY = "current_repo".freeze
   DEFAULT_CLI_CLIENT_KEY = "default_cli_client".freeze
   LAST_SYNCED_AT_KEY = "last_synced_at".freeze
+  AUTO_REVIEW_MODE_KEY = "auto_review_mode".freeze
+  AUTO_REVIEW_DELAY_MIN_KEY = "auto_review_delay_min".freeze
+  AUTO_REVIEW_DELAY_MAX_KEY = "auto_review_delay_max".freeze
 
   CLI_CLIENTS = %w[claude codex opencode].freeze
   DEFAULT_CLI_CLIENT = "claude".freeze
   SYNC_DEBOUNCE_SECONDS = 300 # 5 minutes
+  DEFAULT_AUTO_REVIEW_DELAY_MIN = 5
+  DEFAULT_AUTO_REVIEW_DELAY_MAX = 30
 
   def self.repos_folder
     find_by(key: REPOS_FOLDER_KEY)&.value
@@ -65,5 +70,38 @@ class Setting < ApplicationRecord
     return 0 if last.nil?
     remaining = SYNC_DEBOUNCE_SECONDS - (Time.current - last)
     [ remaining, 0 ].max.to_i
+  end
+
+  def self.auto_review_mode?
+    find_by(key: AUTO_REVIEW_MODE_KEY)&.value == "true"
+  end
+
+  def self.auto_review_mode=(enabled)
+    setting = find_or_initialize_by(key: AUTO_REVIEW_MODE_KEY)
+    setting.update!(value: enabled.to_s)
+  end
+
+  def self.auto_review_delay_min
+    find_by(key: AUTO_REVIEW_DELAY_MIN_KEY)&.value&.to_i || DEFAULT_AUTO_REVIEW_DELAY_MIN
+  end
+
+  def self.auto_review_delay_min=(seconds)
+    setting = find_or_initialize_by(key: AUTO_REVIEW_DELAY_MIN_KEY)
+    setting.update!(value: seconds.to_s)
+  end
+
+  def self.auto_review_delay_max
+    find_by(key: AUTO_REVIEW_DELAY_MAX_KEY)&.value&.to_i || DEFAULT_AUTO_REVIEW_DELAY_MAX
+  end
+
+  def self.auto_review_delay_max=(seconds)
+    setting = find_or_initialize_by(key: AUTO_REVIEW_DELAY_MAX_KEY)
+    setting.update!(value: seconds.to_s)
+  end
+
+  def self.auto_review_delay
+    min = auto_review_delay_min
+    max = auto_review_delay_max
+    rand(min..max)
   end
 end
