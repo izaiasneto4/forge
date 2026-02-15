81c0129f489f4a14f0a60333880fa906b6e1ec81
Izaias Oliveira <izaiasneto.dev@gmail.com>
2026-02-14 21:25:39 -0300

    Pin @hotwired/turbo importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/turbo", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@rails/actioncable", to: "actioncable.esm.js"
